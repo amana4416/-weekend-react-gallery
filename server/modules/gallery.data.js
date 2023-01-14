1d831954f05be216aa482f245e751965d4d6d4e3
Aman Abdirazak <amanabdirizak@icloud.com>
2023-01-14 08:51:16 -0600

    added kitten photos to the array

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/siamese.jpg', description: 'siamese kitten available for adoption', likes: 7 },
+    { id: 3, path: 'images/british_shorthair.jpg', description: 'british shorthair kitten available for adoption', likes: 5 },
+    { id: 4, path: 'images/tabby.jpg', description: 'orange tabby cat availble for adoption', likes: 6}
 ];
 
 module.exports = galleryItems;
