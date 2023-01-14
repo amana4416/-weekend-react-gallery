90314285ab4cf54e636a27430e27f9110908c609
Aman Abdirazak <amanabdirizak@icloud.com>
2023-01-13 18:43:50 -0600

    GalleryList and Header components are working

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+//components
+import Header from '../Header/Header.jsx';
+import GalleryList from '../GalleryList/GalleryList.jsx';
+import { response } from 'express';
+
 
 function App() {
+  const [photo, setPhoto] = useState('');
+
+  useEffect(() => {
+    getPhotos();
+  }, []) //the empty array is a stop sign so we only call the getPhotos() 
+  //function once when the page uploads
+
+  //get request
+  const getPhotos() => {
+    axios.get('/gallery')
+    .then(response =>{
+      
+    })
+  }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
+        <Header />
+        <GalleryList />
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
       </div>
