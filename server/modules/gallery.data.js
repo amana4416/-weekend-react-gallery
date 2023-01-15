418462717bbe2c1bfd129fde82f838665ae70383
Aman Abdirazak <amanabdirizak@icloud.com>
2023-01-15 09:50:31 -0600

    finished styling, used an MUI button, but need more practice with it, im not very good

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,7 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/siamese.jpg', name:'Miso', description: 'siamese kitten available for adoption', likes: 0 },
-    { id: 2, path: 'images/british_shorthair.jpg', name:'Sister Minnie', description: 'british shorthair kitten available for adoption', likes: 0 },
-    { id: 3, path: 'images/tabby.jpg', name:'Simba', description: 'orange tabby cat availble for adoption', likes: 0}
+    { id: 1, path: 'images/siamese.jpg', name:'Miso', description: 'Miso is a 6 week old siamese kitten that loves to cuddle', likes: 0 },
+    { id: 2, path: 'images/british_shorthair.jpg', name:'Sister Minnie', description: 'Sister Minnie is a British shorthair kitten that is from a litter of 6. She loves to play and is often seen on the popular tiktok page @zainah.mb', likes: 0 },
+    { id: 3, path: 'images/tabby.jpg', name:'Simba', description: 'Simba is a tabby kitten that shares only 1 brain cell with all other orange cats in the world', likes: 0},
+    { id: 4, path: 'images/ragdoll.jpg', name:'Oscar', description: 'Oscar is only 7 weeks old and is already extremely sophisticated and classy for his age', likes: 0},
+    { id: 5, path: 'images/siberian.jpg', name:'Angel', description: 'Angel is the type of kitten to get zoomies at 2AM! Beware!', likes: 0},
+    { id: 6, path: 'images/bombay.jpg', name:'Nyx', description: 'Nyx was the runt of her litter, and like a shadow, she will sneak up on you', likes: 0}
 ];
 
 module.exports = galleryItems;
