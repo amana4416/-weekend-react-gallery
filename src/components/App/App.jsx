c190e65f3994e72d1c2c64c448163a26423bfeaf
Aman Abdirazak <amanabdirizak@icloud.com>
2023-01-15 15:28:29 -0600

    able to add new pictures to the gallery

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -34,7 +34,9 @@ function App() {
     return (
       <div className="App">
          <Header />
-         <GalleryForm />
+         <GalleryForm 
+          getPhotos={getPhotos}
+         />
         <GalleryList 
           getPhotos={getPhotos}
           photos={photos}
